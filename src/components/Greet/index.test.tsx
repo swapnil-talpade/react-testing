1aa4de605ea11a838110cd6181d256e967cc0f45
swapnil-talpade <swapniltalpade41@gmail.com>
2022-11-27 12:42:14 +0530

    uses describe

--- src/components/Greet/index.test.tsx
+++ src/components/Greet/index.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Greet from ".";
 
-test("renders greet", () => {
-  render(<Greet />);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
-});
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
 
-test("renders greet with a name", () => {
-  render(<Greet name="Swapnil" />);
-  const textElement = screen.getByText("Hello Swapnil");
-  expect(textElement).toBeInTheDocument();
+  test("renders with a name", () => {
+    render(<Greet name="Swapnil" />);
+    const textElement = screen.getByText("Hello Swapnil");
+    expect(textElement).toBeInTheDocument();
+  });
 });
