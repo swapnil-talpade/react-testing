4f9cf609c63cc104bd09566bb65fcdbc59889954
swapnil-talpade <swapniltalpade41@gmail.com>
2022-12-28 13:37:52 +0530

    tests with getByLabelText

--- src/components/application/index.test.tsx
+++ src/components/application/index.test.tsx
@@ -20,6 +20,9 @@ describe("Application", () => {
     });
     expect(nameElement).toBeInTheDocument();
 
+    const nameElement2 = screen.getByLabelText("Name");
+    expect(nameElement2).toBeInTheDocument();
+
     const bioElement = screen.getByRole("textbox", {
       name: "Bio",
     });
@@ -30,6 +33,11 @@ describe("Application", () => {
 
     const termsElement = screen.getByRole("checkbox");
     expect(termsElement).toBeInTheDocument();
+
+    const termsElement2 = screen.getByLabelText(
+      "I agree to the terms and conditions"
+    );
+    expect(termsElement2).toBeInTheDocument();
 
     const submitElement = screen.getByRole("button");
     expect(submitElement).toBeInTheDocument();
