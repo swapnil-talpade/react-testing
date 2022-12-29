8c70a82ade3a013cf36bd7fe0dedce3d1be8c555
swapnil-talpade <swapniltalpade41@gmail.com>
2022-12-29 15:28:25 +0530

    adds keyboard interactions

--- src/components/counter/index.test.tsx
+++ src/components/counter/index.test.tsx
@@ -52,4 +52,46 @@ describe("counter", () => {
     const countElement = screen.getByRole("heading");
     expect(countElement).toHaveTextContent("2");
   });
+
+  test("should render a count of 10 after clicking the set button", async () => {
+    user.setup();
+
+    render(<Counter />);
+
+    const amountInput = screen.getByRole("spinbutton");
+    await user.type(amountInput, "10");
+    expect(amountInput).toHaveValue(10);
+
+    const setButton = screen.getByRole("button", {
+      name: "Set",
+    });
+
+    await user.click(setButton);
+
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toHaveTextContent("10");
+  });
+
+  test("should focus elements in right order", async () => {
+    user.setup();
+
+    render(<Counter />);
+
+    const amountInput = screen.getByRole("spinbutton");
+    const setButton = screen.getByRole("button", {
+      name: "Set",
+    });
+    const incrementButton = screen.getByRole("button", {
+      name: "Increment",
+    });
+
+    await user.tab();
+    expect(incrementButton).toHaveFocus();
+
+    await user.tab();
+    expect(amountInput).toHaveFocus();
+
+    await user.tab();
+    expect(setButton).toHaveFocus();
+  });
 });
