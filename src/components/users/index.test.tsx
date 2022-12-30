e3cea91d6c170396aca553bb56fe9d1d757fb2cb
swapnil-talpade <swapniltalpade41@gmail.com>
2022-12-30 16:51:58 +0530

    adds msw error handling

--- src/components/users/index.test.tsx
+++ src/components/users/index.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
+import { rest } from "msw";
 
 import Users from ".";
+import { server } from "../../mocks/server";
 
 describe("User", () => {
   test("should render component", () => {
@@ -15,5 +17,20 @@ describe("User", () => {
 
     const users = await screen.findAllByRole("listitem");
     expect(users).toHaveLength(3);
+  });
+
+  test("should render error", async () => {
+    server.use(
+      rest.get(
+        "https://jsonplaceholder.typicode.com/users",
+        (req, res, ctx) => {
+          return res(ctx.status(500));
+        }
+      )
+    );
+    render(<Users />);
+
+    const errorText = await screen.findByText("Error fetching users");
+    expect(errorText).toBeInTheDocument();
   });
 });
