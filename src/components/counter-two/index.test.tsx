21d389ec35dbb733ff4836c4802c5b20c2d99126
swapnil-talpade <swapniltalpade41@gmail.com>
2022-12-30 15:11:06 +0530

    adds mocking functions

---
+++ src/components/counter-two/index.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import CounterTwo from ".";
+
+describe("Counter Two", () => {
+  test("should render count correctly", () => {
+    render(<CounterTwo count={0} />);
+    const textElement = screen.getByText("Counter Two");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test("should call handlers", async () => {
+    user.setup();
+    const incrementHandler = jest.fn();
+    const decrementHandler = jest.fn();
+    render(
+      <CounterTwo
+        count={0}
+        handleIncrement={incrementHandler}
+        handleDecrement={decrementHandler}
+      />
+    );
+
+    const incrementButton = screen.getByRole("button", {
+      name: "Increment",
+    });
+    expect(incrementButton).toBeInTheDocument();
+
+    const decrementButton = screen.getByRole("button", {
+      name: "Decrement",
+    });
+    expect(decrementButton).toBeInTheDocument();
+
+    await user.click(incrementButton);
+    await user.click(decrementButton);
+
+    expect(incrementHandler).toHaveBeenCalledTimes(1);
+    expect(decrementHandler).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/components/counter-two/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { CounterTwoProps } from "./index.types";
+
+const CounterTwo = (props: CounterTwoProps) => {
+  return (
+    <div>
+      <h1>Counter Two</h1>
+      <p>{props.count}</p>
+      {props.handleIncrement && (
+        <button onClick={props.handleIncrement}>Increment</button>
+      )}
+      {props.handleDecrement && (
+        <button onClick={props.handleDecrement}>Decrement</button>
+      )}
+    </div>
+  );
+};
+
+export default CounterTwo;
